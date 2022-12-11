b2726a77c9b04972805438d41c3edddcb5597724
austinpkugler <austinpkugler@gmail.com>
2022-12-11 15:15:26 -0800

    Implement cyk parsing

---
+++ run.py
@@ -0,0 +1,31 @@
+import sys
+
+from CYK import Parser
+
+
+if __name__ == '__main__':
+    while True:
+        print('Enter a CNF grammar G followed by an input word w. Terminals should be lowercase and nonterminals uppercase. Whitespace does not matter.\n')
+
+        print('Examples:')
+        print('G = S -> AB|BC, A -> BA|a, B -> CC|b, C -> AB|a')
+        print('w = babaa\n')
+
+        print('G = S -> AB|BC, A -> SA|a, B -> BB|b')
+        print('w = ababb\n')
+
+        print('G = S -> AB, A -> a, B -> b')
+        print('w = ba\n')
+
+        print('-----------------------------------------------')
+
+        grammar = input('G = ')
+        word = input('w = ')
+
+        print()
+        parser = Parser.Parser(grammar)
+        parser.parse(word)
+        stop = input('\nQuit (Y/n)? ')
+        print()
+        if stop == 'Y':
+            break
